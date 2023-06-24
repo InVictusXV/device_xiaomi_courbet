46913bb0afed5333179b5500c8b0e98317175056
InVictusXV <aradheakiki123@gmail.com>
2023-06-24 13:23:14 +0700

    courbet: Evolution X Bringup

--- evolution_courbet.mk
+++ evolution_courbet.mk
@@ -11,14 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from courbet device
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
-# Inherit some common Ancient OS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+EVO_BUILD_TYPE := ANYA
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := lineage_courbet
+PRODUCT_NAME := evolution_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2101K9AG
